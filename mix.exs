d86d1dde3bc1f692c54e9a4636eaaf70051a84f3
Jakub Pryc <94321002+Noarkhh@users.noreply.github.com>
2026-05-21 17:57:13 +0200

    Merge pull request #21 from membraneframework/fix/apache-license-spdx
    Fix Apache license SPDX identifier in mix.exs

--- mix.exs
+++ mix.exs
@@ -42,7 +42,7 @@ defmodule Membrane.Scissors.Plugin.MixProject do
   defp package do
     [
       maintainers: ["Membrane Team"],
-      licenses: ["Apache 2.0"],
+      licenses: ["Apache-2.0"],
       links: %{
         "GitHub" => @github_url,
         "Membrane Framework Homepage" => "https://membraneframework.org"
